029d056be536c1a9df06f0d656251078dcf31f59
stla <laurent_step@yahoo.fr>
2022-03-28 12:55:28 +0200

    update, push, is, type

--- R/jsonStrings.R
+++ R/jsonStrings.R
@@ -200,6 +200,57 @@ jsonString <- R6Class(
       stopifnot(isJsonString(jstring))
       ptr <- Xptr(jstring)
       private[[".jsonString"]]$update(ptr)
+    },
+
+    #' @description Append an element to the reference JSON string (if it 
+    #'   represents an array).
+    #'
+    #' @param jstring a JSON string
+    #'
+    #' @return Nothing, this updates the reference JSON string.
+    #' 
+    #' @examples 
+    #' jstring <- jsonString$new("[1, 2, 3, 4, 5]")
+    #' jstring2 <- jsonString$new("{\"a\":[4,5,6],\"c\":\"goodbye\"}")
+    #' jstring$push(jstring2)
+    #' jstring
+    push = function(jstring){
+      stopifnot(isJsonString(jstring))
+      ptr <- Xptr(jstring)
+      private[[".jsonString"]]$push(ptr)
+    },
+    
+    #' @description Check the type of the reference JSON string.
+    #'
+    #' @param type the type to be checked, one of \code{"array"}, \code{"object"}, 
+    #'   \code{"string"}, \code{"number"}, \code{"integer"}, \code{"null"}, 
+    #'   \code{"boolean"}
+    #'
+    #' @return A Boolean value.
+    #' 
+    #' @examples 
+    #' jstring <- jsonString$new("{\"a\":[1,2,3],\"b\":\"hello\"}")
+    #' jstring$is("object")
+    #' jstring$is("array")
+    is = function(type){
+      types <- 
+        c("array", "object", "number", "integer", "string", "null", "boolean")
+      type <- match.arg(type, types)
+      private[[".jsonString"]]$is(type) 
+    },
+    
+    #' @description Get the type of the reference JSON string.
+    #'
+    #' @return A character string indicating the type of the JSON string.
+    #' @export
+    #' 
+    #' @examples 
+    #' jstring <- jsonString$new("{\"a\":[1,2,3],\"b\":\"hello\"}")
+    #' jstring$type()
+    #' jstring <- jsonString$new("999")
+    #' jstring$type()
+    type = function(){
+      private[[".jsonString"]]$type() 
     }
     
   )
@@ -207,30 +258,6 @@ jsonString <- R6Class(
 )
 
 
-
-
-
-#' @title Update object
-#' @description Update a JSON string.
-#'
-#' @param jsonstring1 a JSON string representing an object
-#' @param jsonstring2 a JSON string representing an object
-#'
-#' @return A JSON string.
-#' @export
-#'
-#' @examples jstring1 <- jsonString("{\"a\":[1,2,3],\"b\":\"hello\"}")
-#' jstring2 <- jsonString("{\"a\":[4,5,6],\"c\":\"goodbye\"}")
-#' jsonUpdate(jstring1, jstring2)
-jsonUpdate <- function(jsonstring1, jsonstring2){
-  if(is.character(jsonstring1)){
-    jsonstring1 <- jsonString(jsonstring1)
-  }
-  if(is.character(jsonstring2)){
-    jsonstring2 <- jsonString(jsonstring2)
-  }
-  new(JSONPTR, jsonstring1)$update(jsonstring2)
-}
 
 #' @title Patch a JSON string
 #' @description Apply a JSON patch to a JSON string.
@@ -282,65 +309,9 @@ jsonMerge <- function(jsonstring1, jsonstring2){
   new(JSONPTR, jsonstring1)$merge(jsonstring2)
 }
 
-#' @title Append an element
-#' @description Append an element to a JSON string.
-#'
-#' @param jsonstring1 JSON string representing an array
-#' @param jsonstring2 JSON string 
-#'
-#' @return A JSON string.
-#' @export
-#'
-#' @examples jstring1 <- jsonString("[1,2,3]")
-#' jstring2 <- jsonString("{\"a\":\"hello\",\"b\":\"goodbye\"}")
-#' jsonPush(jstring1, jstring2)
-jsonPush <- function(jsonstring1, jsonstring2){
-  if(is.character(jsonstring1)){
-    jsonstring1 <- jsonString(jsonstring1)
-  }
-  if(is.character(jsonstring2)){
-    jsonstring2 <- jsonString(jsonstring2)
-  }
-  new(JSONPTR, jsonstring1)$push(jsonstring2)
-}
 
-#' @title Check type of JSON string 
-#' @description Check the type of a JSON string.
-#'
-#' @param jsonstring a JSON string
-#' @param type the type to be checked, one of \code{"array"}, \code{"object"}, 
-#'   \code{"string"}, \code{"number"}, \code{"integer"}, \code{"null"}, 
-#'   \code{"boolean"}
-#'
-#' @return A logical value.
-#' @export
-#' 
-#' @examples jsonIs("[1,2]", "array")
-jsonIs <- function(jsonstring, type){
-  types <- 
-    c("array", "object", "number", "integer", "string", "null", "boolean")
-  type <- match(match.arg(type, types), types)
-  if(is.character(jsonstring)){
-    jsonstring <- jsonString(jsonstring)
-  }
-  new(JSONPTR, jsonstring)$is(type)
-}
 
-#' @title Type of JSON string 
-#' @description The type of a JSON string.
-#'
-#' @param jsonstring a JSON string
-#'
-#' @return A character string indicating the type of the JSON string.
-#' @export
-#' 
-#' @examples jsonType("[1,2]")
-jsonType <- function(jsonstring){
-  if(is.character(jsonstring)){
-    jsonstring <- jsonString(jsonstring)
-  }
-  new(JSONPTR, jsonstring)$type()
-}
+
 
 # #' @title JSON string to character string
 # #' @description Convert a JSON string to a character string.
